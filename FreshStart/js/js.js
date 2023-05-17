aae58243d5590e939759c98543f3724bf043574c
sasha56610 <105659031+sasha56610@users.noreply.github.com>
2023-05-17 18:02:55 +0300

    Add files via upload

---
+++ FreshStart/js/js.js
@@ -0,0 +1,10 @@
+// let butt1 = document.getElementById("volod2")
+// vol.style.display = "none"
+// document.getElementById("btn").onclick= function () {
+//
+//     if (vol.style.display === "none") {
+//         vol.style.display = "inline";
+//     } else if(vol.style.display !== "none"){
+//         vol.style.display = "none"
+//     }
+// }
